8f705a7b8ca7ff464c9bea77702f91a52736172a
lc00734 <964758948@qq.com>
2019-12-31 10:57:36 +0800

    111

--- src/main/resources/springboot-test-all.sh
+++ src/main/resources/springboot-test-all.sh
@@ -4,7 +4,7 @@ docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker run -p 8089:8089 --name ${app_name} \
+docker run -p 8094:8094 --name ${app_name} \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
 -d mall-tiny/${app_name}:0.0.1-SNAPSHOT
