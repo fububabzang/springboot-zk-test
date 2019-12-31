de6c07ed9ffb7db14f0abf4607ac00ccba4a0cb0
lc00734 <964758948@qq.com>
2019-12-31 11:11:46 +0800

    111

--- src/main/java/com/realme/zookeeper/api/WatcherApi.java
+++ src/main/java/com/realme/zookeeper/api/WatcherApi.java
@@ -2,8 +2,11 @@ package com.realme.zookeeper.api;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * @program: zk-test
@@ -14,10 +17,26 @@ import org.slf4j.LoggerFactory;
 public class WatcherApi implements Watcher {
     private static final Logger logger = LoggerFactory.getLogger(WatcherApi.class);
 
+    private static ZooKeeper zooKeeper = null;
+
     @Override
+    public void process(WatchedEvent event) {
+        if (event.getState() == Event.KeeperState.Expired) {
+            try {
+                zooKeeper = new ZooKeeper("115.28.211.190:2181", 3000, this);
+                logger.info("【Watcher监听事件】={}",event.getState());
+                logger.info("【监听路径为】={}",event.getPath());
+                logger.info("【监听的类型为】={}",event.getType()); //  三种监听类型： 创建，删除，更新
+            } catch (IOException e) {
+                logger.warn("fail to connect to zoo keeper", e);
+            }
+        }
+    }
+
+   /* @Override
     public void process(WatchedEvent event) {
         logger.info("【Watcher监听事件】={}",event.getState());
         logger.info("【监听路径为】={}",event.getPath());
         logger.info("【监听的类型为】={}",event.getType()); //  三种监听类型： 创建，删除，更新
-    }
+    }*/
 }
